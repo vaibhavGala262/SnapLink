b6725a516115d3c5445011de3295e3a2f86c8af1
Vaibhav Gala <vaibhavgala262@gmail.com>
2025-08-16 03:46:36 +0530

    Removed use of env from local .env

--- src/main/java/com/vaibhavgala/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/vaibhavgala/url_shortner/UrlShortnerApplication.java
@@ -10,9 +10,7 @@ public class UrlShortnerApplication {
 	public static void main(String[] args) {
 
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(UrlShortnerApplication.class, args);
 	}
 
